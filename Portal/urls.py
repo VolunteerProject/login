bee30004ff5c615ecc9a038dcce7d6d6107e24b5
UJJWAL KUMAR <44162744+Ujjwal110599@users.noreply.github.com>
2020-05-11 00:54:24 +0530

    Update urls.py

--- Portal/urls.py
+++ Portal/urls.py
@@ -7,8 +7,6 @@ from . import views
 
 urlpatterns=[
     path('login/',views.userlogin,name='login'),
-    path('',views.index,name='index'),
-
     path('', include('social_django.urls', namespace='social')),  # for signing up with google accounts
     path('',include("django.contrib.auth.urls")),  # for authentication with google accounts
     path('logout/', logout, {'next_page': settings.LOGOUT_REDIRECT_URL} , name='logout'),
