f1f43101f42fcbdae7c59881740e8dc39ed00931
UJJWAL KUMAR <44162744+Ujjwal110599@users.noreply.github.com>
2020-05-11 13:31:55 +0530

    Update settings.py

--- VolunteerNetwork/settings.py
+++ VolunteerNetwork/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Portal',
+    'social_django',
 ]
 
 # we’re going to use Google accounts to establish the authentication,
@@ -96,12 +97,6 @@ WSGI_APPLICATION = 'VolunteerNetwork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
