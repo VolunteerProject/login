8782027c1165541f8efc3a7aad1ab1a44d05f8ef
UJJWAL KUMAR <44162744+Ujjwal110599@users.noreply.github.com>
2020-05-16 03:53:51 +0530

    Update viewsets.py

---
+++ Portal/api/viewsets.py
@@ -0,0 +1,10 @@
+from Portal.api.serializers import OrgSerializer
+from Portal.models import Organization
+from rest_framework import viewsets
+
+
+class PortalViewSet(viewsets.ModelViewSet):
+    queryset = Organization.objects.all()
+    serializer_class = OrgSerializer
+
+
