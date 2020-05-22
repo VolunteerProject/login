280de08f3d6b9d1f3f8d91d29603a83fe04f2cbe
UJJWAL KUMAR <44162744+Ujjwal110599@users.noreply.github.com>
2020-05-22 12:35:26 +0530

    Update urls.py

--- VolunteerNetwork/Portal/urls.py
+++ VolunteerNetwork/Portal/urls.py
@@ -12,7 +12,7 @@ router.register(r'token_save', views.VolunteerViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 
     
     
